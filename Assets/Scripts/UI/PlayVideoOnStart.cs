efb289839a5470420cbe1451774f1059e3004a16
juliapetrie <julia.petrie@icloud.com>
2025-03-26 00:57:13 -0700

    UI adjustments to main menu and loading scene
    - created new loading scene with pacman animation
    - hooked this up in build settings to transition using preload - Julia instead
    - continued adjusting start menu and replaced previous copy with StartMenu - Julia in build settings
    - major script adjustments to scene handler to work with the video instead of static black panel
    - added video related scripts to scripts/UI
    - some changed to initial loading scene so making PR now

--- Assets/Scripts/UI/PlayVideoOnStart.cs
+++ Assets/Scripts/UI/PlayVideoOnStart.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Video;
+using System.Collections;
 
 public class PlayVideoOnStart : MonoBehaviour
 {
@@ -7,15 +8,29 @@ public class PlayVideoOnStart : MonoBehaviour
 
     void Start()
     {
-        videoPlayer.playOnAwake = false;  
-        videoPlayer.time = 0;             
+        videoPlayer.playOnAwake = false;
+        videoPlayer.isLooping = false;
+        videoPlayer.time = 0;
+        videoPlayer.playbackSpeed = 1f;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.None;
+
         videoPlayer.prepareCompleted += OnVideoPrepared;
-        videoPlayer.Prepare();            // Begin vid prep
+        videoPlayer.Prepare();
     }
 
     void OnVideoPrepared(VideoPlayer vp)
     {
-        vp.time = 0;   // video at start
-        vp.Play();     // vid plays at normal speed
+        StartCoroutine(WarmUpAndPlay());
+    }
+
+    IEnumerator WarmUpAndPlay()
+    {
+        videoPlayer.Play(); // start decoding
+        yield return new WaitForEndOfFrame(); // load first frame to prevent buffering issues
+        videoPlayer.Pause(); // pause 
+        videoPlayer.time = 0; // reset clean
+        yield return new WaitForSeconds(0.2f); // delayed as it kept starting early
+        videoPlayer.Play();
     }
 }
+
