9af23a68f95ac2dc766952d38d26525c8723189f
juliapetrie <julia.petrie@icloud.com>
2025-03-26 22:32:01 -0700

    countdown controller and tutorial panel manager
    - tutorial panel manager controls if tutorial appears or not (it only appears in Level 1 scene)
    - count down controller disables game play until 3, 2, 1 is complete by disabling ghosts and player movement.

---
+++ Assets/Scripts/TutorialPanelManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialPanelController : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorialPanel; 
+     [SerializeField] private CountdownController countdownController; 
+
+    private void Start()
+    {
+        ShowTutorialIfLevel1();
+    }
+
+    private void ShowTutorialIfLevel1()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName == "Level 1")
+        {
+            Debug.Log("tutorial panel only for level1");
+            tutorialPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("not level 1");
+            tutorialPanel.SetActive(false);
+        }
+    }
+
+      public void CloseTutorialPanel()
+    {
+        if (tutorialPanel != null)
+        {
+            tutorialPanel.SetActive(false);
+            Debug.Log("Tutorial Panel Closed.");
+             if (countdownController != null)
+            {
+                countdownController.DelayedCountDown();
+            }
+        }
+
+    }
+}
