2476ef85580c32be7a56beccc6ce5454d509a7d7
juliapetrie <julia.petrie@icloud.com>
2025-03-30 01:49:59 -0700

    added flashing effect and game play elements
    - pacman flashes when having powerup
    - pacman also flashes for 1.5f as a cooldown period after hit to prevent multiple hits at once

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -6,43 +6,66 @@ public class LivesManager : MonoBehaviour
     [SerializeField] private GameObject threeLivesIcon;
     [SerializeField] private GameObject twoLivesIcon;
     [SerializeField] private GameObject oneLifeIcon;
-    // [SerializeField] private string level1Name = "Merged Level 1 V1"; //update if we change Level 1 scene to different name
 
-    private int lives = 3;
+    private int lives;
 
     private void Start()
     {
-        lives = 3; 
+        lives = 3;
         UpdateLivesUI();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Ghost")) // ensure ghosts are all tagged
+        if (other.CompareTag("Ghost")) 
         {
             LoseLife();
         }
     }
 
- public void LoseLife()
-{
-    if (lives <= 0) return;
-
-    lives--;
-
-    Debug.Log($"lives: {lives}");
-
-    UpdateLivesUI();
-
-    if (lives <= 0)
+    public void LoseLife()
     {
-        Debug.Log("dead");
+        if (lives <= 0) return;
 
-        FindFirstObjectByType<GameManager>()?.TriggerGameOver();
+        lives--;
+
+        Debug.Log($"lives: {lives}");
+        UpdateLivesUI();
+
+        if (lives <= 0)
+        {
+            Debug.Log("dead");
+            FindFirstObjectByType<GameManager>()?.TriggerGameOver();
+        }
+        else
+        {
+            GameObject pacman = GameObject.FindGameObjectWithTag("Pacman"); //cooldown delay to give chance to escape
+            if (pacman != null)
+            {
+                PlayerController playerController = pacman.GetComponent<PlayerController>();
+                PacmanFlashingEffect flash = pacman.GetComponent<PacmanFlashingEffect>();
+
+                if (playerController != null)
+                    playerController.hasPelletPowerup = true;
+
+                if (flash != null)
+                    flash.StartFlashing(1.5f); // 1.5 second of invincibility
+
+                StartCoroutine(ResetInvincibility(pacman, 1.5f));
+            }
+        }
     }
-}
 
+    private System.Collections.IEnumerator ResetInvincibility(GameObject pacman, float delay)
+    {
+        yield return new WaitForSeconds(delay);
 
+        PlayerController playerController = pacman.GetComponent<PlayerController>();
+        if (playerController != null)
+            playerController.hasPelletPowerup = false;
+
+        Debug.Log("reset to normal powers");
+    }
 
     private void UpdateLivesUI()
     {
--- Assets/Scripts/itemCollection.cs
+++ Assets/Scripts/itemCollection.cs
@@ -1,3 +1,97 @@
+// using UnityEngine;
+// using UnityEngine.Events;
+// using System.Collections;
+
+// public class itemCollection : MonoBehaviour
+// {
+//     public float sprintDuration = 2f;
+//     public float pelletDuration = 8f;
+//     public UnityEvent OnPelletCollected = new();
+//     [SerializeField] private PlayerController playerController;
+//     [SerializeField] private PelletCounterUI pelletCounterUI;
+
+//     private void Start()
+//     {
+//         playerController = GetComponent<PlayerController>();
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+
+//         //disable the collider to avoid duplicate collisions
+//         Collider collider = other.GetComponent<Collider>();
+//         if (collider != null)
+//         {
+//             collider.enabled = false;
+//         }
+
+//         if (other.transform.tag == "Pellet")
+//         {
+//             Debug.Log("pellet collected");
+//             Destroy(other.gameObject);
+//             OnPelletCollected?.Invoke();
+//             pelletCounterUI?.UpdateScore(); // Update score
+//         }
+//         else if (other.transform.tag == "Fruit")
+//         {
+//             Debug.Log("fruit collected");
+//             Destroy(other.gameObject);
+//             StartCoroutine(GiveSprintPowerup());
+//         }
+//         else if (other.transform.tag == "PowerPellet")
+//         {
+//             Debug.Log("power pellet collected");
+//             Destroy(other.gameObject);
+//             StartCoroutine(GivePelletPowerup());
+//         }
+//     }
+
+//     // private IEnumerator GivePelletPowerup()
+//     // {
+//     //     if (playerController != null)
+//     //     {
+//     //         playerController.hasPelletPowerup = true;
+//     //         yield return new WaitForSeconds(pelletDuration);
+//     //         playerController.hasPelletPowerup = false;
+//     //     }
+//     // }
+
+// private IEnumerator GivePelletPowerup()
+// {
+//     if (playerController != null)
+//     {
+//         playerController.hasPelletPowerup = true;
+
+//         //  Start flashing effect
+//         PacmanFlash pacmanFlash = playerController.GetComponent<PacmanFlash>();
+//         if (pacmanFlash != null)
+//             pacmanFlash.StartFlashing(pelletDuration);
+
+//         Debug.Log("Pac-Man is now INVINCIBLE.");
+
+//         yield return new WaitForSeconds(pelletDuration);
+
+//         playerController.hasPelletPowerup = false;
+
+//         if (pacmanFlash != null)
+//             pacmanFlash.StopFlashing();
+
+//         Debug.Log("Pac-Man is vulnerable again.");
+//     }
+// }
+
+//     private IEnumerator GiveSprintPowerup()
+//     {
+//         if (playerController != null)
+//         {
+//             playerController.hasSprintPowerup = true;
+//             yield return new WaitForSeconds(sprintDuration);
+//             playerController.hasSprintPowerup = false;
+//         }
+//     }
+
+// }
+
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
@@ -7,6 +101,7 @@ public class itemCollection : MonoBehaviour
     public float sprintDuration = 2f;
     public float pelletDuration = 8f;
     public UnityEvent OnPelletCollected = new();
+    
     [SerializeField] private PlayerController playerController;
     [SerializeField] private PelletCounterUI pelletCounterUI;
 
@@ -17,28 +112,26 @@ public class itemCollection : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
-        //disable the collider to avoid duplicate collisions
         Collider collider = other.GetComponent<Collider>();
         if (collider != null)
         {
             collider.enabled = false;
         }
 
-        if (other.transform.tag == "Pellet")
+        if (other.CompareTag("Pellet"))
         {
             Debug.Log("pellet collected");
             Destroy(other.gameObject);
             OnPelletCollected?.Invoke();
-            pelletCounterUI?.UpdateScore(); // Update score
+            pelletCounterUI?.UpdateScore();
         }
-        else if (other.transform.tag == "Fruit")
+        else if (other.CompareTag("Fruit"))
         {
             Debug.Log("fruit collected");
             Destroy(other.gameObject);
             StartCoroutine(GiveSprintPowerup());
         }
-        else if (other.transform.tag == "PowerPellet")
+        else if (other.CompareTag("PowerPellet"))
         {
             Debug.Log("power pellet collected");
             Destroy(other.gameObject);
@@ -51,8 +144,22 @@ public class itemCollection : MonoBehaviour
         if (playerController != null)
         {
             playerController.hasPelletPowerup = true;
+
+            //  flashing effect
+            PacmanFlashingEffect flash = playerController.GetComponent<PacmanFlashingEffect>();
+            if (flash != null)
+                flash.StartFlashing(pelletDuration);
+
+            Debug.Log("Pacman invincible.");
+
             yield return new WaitForSeconds(pelletDuration);
+
             playerController.hasPelletPowerup = false;
+
+            if (flash != null)
+                flash.StopFlashing();
+
+            Debug.Log("pacman back to normal powers");
         }
     }
 
@@ -65,5 +172,5 @@ public class itemCollection : MonoBehaviour
             playerController.hasSprintPowerup = false;
         }
     }
-
 }
+
